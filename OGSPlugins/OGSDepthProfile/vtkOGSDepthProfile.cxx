74556402f1c46a98bcbaff135784ffe0929bfe53
Arnau Miro <amiro@inogs.it>
2018-09-26 11:06:37 +0200

    First commit of the OGSDepthProfile filter. Computes a profile in a ParaView mesh and returns the plot.

---
+++ OGSPlugins/OGSDepthProfile/vtkOGSDepthProfile.cxx
@@ -0,0 +1,234 @@
+/*=========================================================================
+
+  Program:   OGSDepthProfile
+  Module:    vtkOGSDepthProfile.cxx
+
+  Copyright (c) 2018 Arnau Miro, OGS
+  All rights reserved.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#include "vtkOGSDepthProfile.h"
+
+#include "vtkCell.h"
+#include "vtkCellData.h"
+#include "vtkCellLocator.h"
+#include "vtkCharArray.h"
+#include "vtkGenericCell.h"
+#include "vtkPointData.h"
+#include "vtkPointSet.h"
+#include "vtkDataSet.h"
+#include "vtkExecutive.h"
+#include "vtkSmartPointer.h"
+#include "vtkInformation.h"
+#include "vtkInformationVector.h"
+#include "vtkStaticCellLocator.h"
+#include "vtkObjectFactory.h"
+
+#define CELL_TOLERANCE_FACTOR_SQR 1e-6
+
+vtkStandardNewMacro(vtkOGSDepthProfile);
+vtkCxxSetObjectMacro(vtkOGSDepthProfile, CellLocatorPrototype, vtkAbstractCellLocator);
+
+//----------------------------------------------------------------------------
+vtkOGSDepthProfile::vtkOGSDepthProfile() {
+	this->SetNumberOfInputPorts(2);
+	this->SetNumberOfOutputPorts(1);
+
+	this->MaskPoints = nullptr;
+	this->PointList  = nullptr;
+	this->CellList   = nullptr;
+
+	this->CellLocatorPrototype = nullptr;
+
+	this->ValidPointMaskArrayName = nullptr;
+	this->SetValidPointMaskArrayName("vtkValidPointMask");
+}
+
+//----------------------------------------------------------------------------
+vtkOGSDepthProfile::~vtkOGSDepthProfile() {
+	delete this->PointList;
+	delete this->CellList;
+
+	this->vtkOGSDepthProfile::SetValidPointMaskArrayName(nullptr);
+	this->vtkOGSDepthProfile::SetCellLocatorPrototype(nullptr);
+
+	if (this->MaskPoints)
+		this->MaskPoints->Delete();
+}
+
+//----------------------------------------------------------------------------
+int vtkOGSDepthProfile::RequestData(vtkInformation *vtkNotUsed(request), 
+	vtkInformationVector **inputVector, vtkInformationVector *outputVector) {
+
+	// get the info objects
+	vtkInformation *inInfo = inputVector[0]->GetInformationObject(0);
+	vtkInformation *sourceInfo = inputVector[1]->GetInformationObject(0);
+	vtkInformation *outInfo = outputVector->GetInformationObject(0);
+
+	// get the input and output
+	// input contains the interpolating line information (number of points, etc)
+	vtkDataSet *input = vtkDataSet::SafeDownCast(
+		inInfo->Get(vtkDataObject::DATA_OBJECT()));
+	// Source contains the data where to interpolate from
+	vtkDataSet *source = vtkDataSet::SafeDownCast(
+		sourceInfo->Get(vtkDataObject::DATA_OBJECT()));
+	vtkDataSet *output = vtkDataSet::SafeDownCast(
+		outInfo->Get(vtkDataObject::DATA_OBJECT()));
+
+	// First, copy the input to the output as a starting point
+	output->CopyStructure(input);
+
+	// If there is data to interpolate, begin the interpolation
+	if (source) {
+		this->BuildFieldList(source);
+		this->InitializeForProbing(input, output);
+		this->Interpolate(input, source, output);
+	}
+
+	return 1;
+}
+
+//----------------------------------------------------------------------------
+void vtkOGSDepthProfile::BuildFieldList(vtkDataSet* source) {
+  delete this->PointList;
+  delete this->CellList;
+
+  this->PointList = new vtkDataSetAttributes::FieldList(1);
+  this->PointList->InitializeFieldList(source->GetPointData());
+
+  this->CellList = new vtkDataSetAttributes::FieldList(1);
+  this->CellList->InitializeFieldList(source->GetCellData());
+}
+
+void vtkOGSDepthProfile::InitializeForProbing(vtkDataSet* input,vtkDataSet* output) {
+
+	vtkIdType numPts = input->GetNumberOfPoints();
+
+	// if this is repeatedly called by the pipeline for a composite mesh,
+	// you need a new array for each block
+	// (that is you need to reinitialize the object)
+	if (this->MaskPoints)
+		this->MaskPoints->Delete();
+
+	this->MaskPoints = vtkCharArray::New();
+	this->MaskPoints->SetNumberOfComponents(1);
+	this->MaskPoints->SetNumberOfTuples(numPts);
+	this->MaskPoints->FillValue(0);
+	this->MaskPoints->SetName(this->ValidPointMaskArrayName ? 
+		this->ValidPointMaskArrayName : "vtkValidPointMask");
+
+	// Allocate storage for output PointData
+	// All input PD is passed to output as PD. Those arrays in input CD that are
+	// not present in output PD will be passed as output PD.
+	vtkPointData* outPD = output->GetPointData();
+	outPD->InterpolateAllocate((*this->PointList), numPts, numPts);
+	// We assume we either have points or cells
+	outPD->InterpolateAllocate((*this->CellList), numPts, numPts);
+
+	this->InitializeOutputArrays(outPD, numPts);
+	outPD->AddArray(this->MaskPoints);
+}
+
+void vtkOGSDepthProfile::InitializeOutputArrays(vtkPointData *outPD, vtkIdType numPts) {
+	for (int i = 0; i < outPD->GetNumberOfArrays(); ++i) {
+		vtkDataArray* da = outPD->GetArray(i);
+		if (da) {
+			da->SetNumberOfTuples(numPts);
+			da->Fill(0);
+		}
+	}
+}
+
+void vtkOGSDepthProfile::Interpolate(vtkDataSet *input, vtkDataSet *source, vtkDataSet *output) {
+
+	int subId;
+	double xyz[3], pcoords[3], closestPoint[3], *weights;
+
+	double tol2 = 400;
+
+	// Preallocate weights
+	weights = new double[source->GetMaxCellSize()];
+
+	// Create the cell locator object
+	vtkSmartPointer<vtkCellLocator> cellLocator = vtkSmartPointer<vtkCellLocator>::New();
+	cellLocator->SetDataSet(source);
+	cellLocator->BuildLocator();
+
+	char* maskArray = this->MaskPoints->GetPointer(0);
+
+	// Recover the number of points in the input
+	int npoints = input->GetNumberOfPoints();
+
+	// Loop the number of points
+	vtkNew<vtkGenericCell> gcell;
+	int abort = 0;
+	for (int ii = 0; ii < npoints && !abort; ii++) {
+		// Update progress
+		this->UpdateProgress((double)(ii)/(double)(npoints));
+		abort = GetAbortExecute();
+
+		// skip points which have already been probed with success.
+		// This is helpful for multiblock dataset probing.
+		if (maskArray[ii] == static_cast<char>(1))
+			continue;
+
+		// Get the xyz coordinate of the point in the input dataset
+		input->GetPoint(ii, xyz);
+
+		// Find the cell that contains xyz and get it
+		vtkIdType cellId;
+		double dist2;
+		cellLocator->FindClosestPoint(xyz, closestPoint, gcell.GetPointer(),cellId,subId,dist2);
+
+		// Evaluate interpolation weights
+		vtkCell* cell = nullptr;
+		if (cellId >= 0) {
+			cell = source->GetCell(cellId);
+			// Compute a tolerance proportional to the cell length.
+			cell->EvaluatePosition(xyz, closestPoint, subId, pcoords, dist2, weights);
+			// Abort if the distance is too big
+			if (dist2 > (cell->GetLength2() * CELL_TOLERANCE_FACTOR_SQR))
+				continue;
+		}
+
+		// Check if the cell has been found
+		if (cell) {
+			// Interpolate point array data
+			output->GetPointData()->InterpolatePoint(
+					(*this->PointList), source->GetPointData(), 0, ii, cell->PointIds, weights
+				);
+			// Interpolate cell array data
+			output->GetPointData()->InterpolatePoint(
+					(*this->CellList), source->GetCellData(), 0, ii, cell->PointIds, weights
+				);			
+			// Update mask array
+			maskArray[ii] = static_cast<char>(1);
+		}
+	}
+
+	this->MaskPoints->Modified();
+	delete [] weights;
+}
+
+//----------------------------------------------------------------------------
+void vtkOGSDepthProfile::SetSourceConnection(vtkAlgorithmOutput* algOutput) {
+	this->SetInputConnection(1, algOutput);
+}
+
+void vtkOGSDepthProfile::SetSourceData(vtkDataObject *input) {
+	this->SetInputData(1, input);
+}
+
+vtkDataObject *vtkOGSDepthProfile::GetSource() {
+	if (this->GetNumberOfInputConnections(1) < 1)
+		return nullptr;
+
+	return this->GetExecutive()->GetInputData(1, 0);
+}
+
+//----------------------------------------------------------------------------
---
+++ OGSPlugins/OGSDepthProfile/vtkOGSDepthProfile.h
@@ -0,0 +1,89 @@
+// -*- c++ -*-
+/*=========================================================================
+
+  Program:   OGSDepthProfile
+  Module:    vtkOGSDepthProfile.h
+
+  Copyright (c) 2018 Arnau Miro, OGS
+  All rights reserved.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#ifndef vtkOGSDepthProfile_h
+#define vtkOGSDepthProfile_h
+
+#include "vtkOGSDepthProfile.h"
+
+#include "vtkDataSetAlgorithm.h"
+#include "vtkDataSetAttributes.h"
+
+class vtkCharArray;
+class vtkPointData;
+class vtkAbstractCellLocator;
+class vtkStaticCellLocator;
+
+class VTK_EXPORT vtkOGSDepthProfile : public vtkDataSetAlgorithm 
+{
+public:
+  static vtkOGSDepthProfile* New();
+  vtkTypeMacro(vtkOGSDepthProfile, vtkDataSetAlgorithm);
+
+  // Description:
+  //Specify the data set that will be probed at the input points.
+  //The Input gives the geometry (the points and cells) for the output,
+  //while the Source is probed (interpolated) to generate the scalars,
+  //vectors, etc. for the output points based on the point locations.
+  void SetSourceData(vtkDataObject *source);
+  vtkDataObject *GetSource();
+  void SetSourceConnection(vtkAlgorithmOutput* algOutput);
+
+  // Description:
+  //Returns the name of the char array added to the output with values 1 for
+  //valid points and 0 for invalid points.
+  //Set to "vtkValidPointMask" by default.
+  vtkSetStringMacro(ValidPointMaskArrayName)
+  vtkGetStringMacro(ValidPointMaskArrayName)
+
+  // Description
+  //Set/Get the prototype cell locator to use for probing the source dataset.
+  //By default, vtkStaticCellLocator will be used.
+   virtual void SetCellLocatorPrototype(vtkAbstractCellLocator*);
+   vtkGetObjectMacro(CellLocatorPrototype, vtkAbstractCellLocator);
+
+protected:
+  vtkOGSDepthProfile();
+  ~vtkOGSDepthProfile() override;
+
+  int RequestData(vtkInformation *, vtkInformationVector **,vtkInformationVector *) override;
+
+  void Interpolate(vtkDataSet *input, vtkDataSet *source, vtkDataSet *output);
+
+  void BuildFieldList(vtkDataSet* source);
+
+  virtual void InitializeForProbing(vtkDataSet *input, vtkDataSet *output);
+  virtual void InitializeOutputArrays(vtkPointData *outPD, vtkIdType numPts);
+
+  void DoProbing(vtkDataSet *input, int srcIdx, vtkDataSet *source,vtkDataSet *output);
+
+  vtkCharArray* MaskPoints;
+
+  vtkAbstractCellLocator* CellLocatorPrototype;
+
+  vtkDataSetAttributes::FieldList* CellList;
+  vtkDataSetAttributes::FieldList* PointList;
+
+private:
+  vtkOGSDepthProfile(const vtkOGSDepthProfile&) = delete;
+  void operator=(const vtkOGSDepthProfile&) = delete;
+
+  char *ValidPointMaskArrayName;
+
+  class vtkVectorOfArrays;
+  vtkVectorOfArrays* CellArrays;
+};
+
+#endif
