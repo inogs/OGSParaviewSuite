a52bd68108eeb3eb03cd1015b2e708cddf558b27
Arnau Miro <amiro@inogs.it>
2018-10-16 14:45:07 +0200

    Added computation of OGSQCriterion

---
+++ OGSPlugins/_OGSQCriterion/vtkOGSComputeQCriterion.h
@@ -0,0 +1,57 @@
+// -*- c++ -*-
+/*=========================================================================
+
+  Program:   OGSComputeQCriterion
+  Module:    vtkOGSComputeQCriterion.h
+
+  Copyright (c) 2018 Arnau Miro, OGS
+  All rights reserved.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#ifndef vtkOGSComputeQCriterion_h
+#define vtkOGSComputeQCriterion_h
+
+#include "vtkDataSet.h"
+#include "vtkRectilinearGridAlgorithm.h"
+
+class VTK_EXPORT vtkOGSComputeQCriterion : public vtkRectilinearGridAlgorithm
+{
+public:
+  static vtkOGSComputeQCriterion* New();
+  vtkTypeMacro(vtkOGSComputeQCriterion, vtkRectilinearGridAlgorithm);
+
+  // Description:
+  // Get the coefficient for the Okubo-Weiss mask
+  vtkSetMacro(coef, double);
+  vtkGetMacro(coef, double);
+
+  // Description:
+  // Gradient method for computing Okubo-Weiss
+  vtkSetMacro(grad_type, int);
+  vtkGetMacro(grad_type, int);
+
+  // Description:
+  // Get the name of the velocity field
+  vtkSetStringMacro(field);
+
+protected:
+  vtkOGSComputeQCriterion();
+  ~vtkOGSComputeQCriterion() override;
+
+  int RequestData(vtkInformation *, vtkInformationVector **, vtkInformationVector *) override;
+
+private:
+  vtkOGSComputeQCriterion(const vtkOGSComputeQCriterion&) = delete;
+  void operator=(const vtkOGSComputeQCriterion&) = delete;
+
+  char *field;
+  double coef;
+  int grad_type;
+};
+
+#endif
