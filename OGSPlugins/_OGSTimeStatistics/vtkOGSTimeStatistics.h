491186387811ab797307707c125d6117c2d324f9
Arnau Miro <amiro@inogs.it>
2018-10-18 13:25:51 +0200

    Programmed the basics of the time integrator and how to accumulate the statistics

--- OGSPlugins/_OGSTimeStatistics/vtkOGSTimeStatistics.h
+++ OGSPlugins/_OGSTimeStatistics/vtkOGSTimeStatistics.h
@@ -15,7 +15,7 @@
 #ifndef vtkOGSTimeStatistics_h
 #define vtkOGSTimeStatistics_h
 
-#include "vtkDataArraySelection.h"
+#include "vtkStringArray.h"
 #include "vtkDataSetAlgorithm.h"
 
 class VTK_EXPORT vtkOGSTimeStatistics : public vtkDataSetAlgorithm
@@ -24,17 +24,31 @@ public:
   static vtkOGSTimeStatistics* New();
   vtkTypeMacro(vtkOGSTimeStatistics, vtkDataSetAlgorithm);
 
+  void SetStartTime(const char *tstep);
+  void SetEndTime(const char *tstep);
+
+  vtkStringArray *GetTimeValues();
 
 protected:
   vtkOGSTimeStatistics();
   ~vtkOGSTimeStatistics();
 
   int RequestInformation(vtkInformation*, vtkInformationVector**, vtkInformationVector*) override;
+  int RequestUpdateExtent(vtkInformation*, vtkInformationVector**, vtkInformationVector*) override;
   int RequestData(vtkInformation *, vtkInformationVector **,vtkInformationVector *) override;
+
+  void InitializeStatistics(vtkDataSet *input, vtkDataSet *output);
+  void AccumulateStatistics(vtkDataSet *input, vtkDataSet *output);
+  void FinalizeStatistics(vtkDataSet *input, vtkDataSet *output);
+
+  vtkStringArray *TimeValues;
 
 private:
   vtkOGSTimeStatistics(const vtkOGSTimeStatistics&) = delete;
   void operator=(const vtkOGSTimeStatistics&) = delete;
+
+  int current_time, start_time, end_time;
+  int abort;
 
 };
 
