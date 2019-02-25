926ad5a4c2e04799d417a76674675ff88daa8dd8
Arnau Miró <arnaumiro@luvarusimperialis.inogs.it>
2019-02-25 09:49:36 +0100

    Adapted to the new basins and coast masks

--- OGSPlugins/OGSSelectTools/vtkOGSSelectBasin.cxx
+++ OGSPlugins/OGSSelectTools/vtkOGSSelectBasin.cxx
@@ -12,6 +12,7 @@
 
 =========================================================================*/
 
+#include "vtkTypeUInt8Array.h"
 #include "vtkFloatArray.h"
 #include "vtkDoubleArray.h"
 #include "vtkCellData.h"
@@ -25,6 +26,8 @@
 
 #include "vtkObjectFactory.h"
 
+#include <cstdint>
+
 vtkStandardNewMacro(vtkOGSSelectBasin);
 
 //----------------------------------------------------------------------------
@@ -32,8 +35,8 @@ vtkStandardNewMacro(vtkOGSSelectBasin);
 /*
 	Macro to set the array precision 
 */
-#define FLDARRAY double
-#define VTKARRAY vtkDoubleArray
+#define FLDARRAY uint8_t
+#define VTKARRAY vtkTypeUInt8Array
 
 #include "../_utils/field.h"
 #include "../_utils/vtkFields.hpp"
@@ -113,18 +116,17 @@ int vtkOGSSelectBasin::RequestData(
 	field::Field<FLDARRAY> mask = VTK::createFieldfromVTK<VTKARRAY,FLDARRAY>(vtkmask);
 
 	// Generate a new field (initialized at zero) that will be used as cutting mask
-	field::Field<FLDARRAY> cutmask(mask.get_n(),1,0.);
+	field::Field<FLDARRAY> cutmask(mask.get_n(),1);
 
 	this->UpdateProgress(0.2);
 
 	// Loop and update cutting mask (Mesh loop, can be parallelized)
 	field::Field<FLDARRAY>::iterator it_mask, it_cut;
 	for (it_mask = mask.begin(), it_cut = cutmask.begin(); it_mask != mask.end(); ++it_mask,++it_cut) {
+		it_cut[0] = 0;
 		// Loop on the basins array selection
-		for (int bid=0; bid < this->GetNumberOfBasinsArrays(); bid++)
-			if ( this->GetBasinsArrayStatus(this->GetBasinsArrayName(bid)) &&
-				 std::fabs(it_mask[0] - (double)(bid+1)) < 1.e-3 ) 
-				it_cut[0] = 1.;
+		for (int bid=0; bid < this->GetNumberOfBasinsArrays(); ++bid)
+			if (this->GetBasinsArrayStatus(this->GetBasinsArrayName(bid)) && it_mask[bid]) { it_cut[0] = 1; continue; }
 	}
 
 	// Convert field to vtkArray and add it to input
--- OGSPlugins/OGSSelectTools/vtkOGSSelectCoast.cxx
+++ OGSPlugins/OGSSelectTools/vtkOGSSelectCoast.cxx
@@ -12,7 +12,9 @@
 
 =========================================================================*/
 
+#include "vtkTypeUInt8Array.h"
 #include "vtkFloatArray.h"
+#include "vtkDoubleArray.h"
 #include "vtkCellData.h"
 #include "vtkPointData.h"
 #include "vtkDataArraySelection.h"
@@ -24,6 +26,8 @@
 
 #include "vtkObjectFactory.h"
 
+#include <cstdint>
+
 vtkStandardNewMacro(vtkOGSSelectCoast);
 
 //----------------------------------------------------------------------------
@@ -31,8 +35,8 @@ vtkStandardNewMacro(vtkOGSSelectCoast);
 /*
 	Macro to set the array precision 
 */
-#define FLDARRAY double
-#define VTKARRAY vtkDoubleArray
+#define FLDARRAY uint8_t
+#define VTKARRAY vtkTypeUInt8Array
 
 #include "../_utils/field.h"
 #include "../_utils/vtkFields.hpp"
@@ -98,18 +102,20 @@ int vtkOGSSelectCoast::RequestData(
 	field::Field<FLDARRAY> mask = VTK::createFieldfromVTK<VTKARRAY,FLDARRAY>(vtkmask);
 
 	// Generate a new field (initialized at zero) that will be used as cutting mask
-	field::Field<FLDARRAY> cutmask(mask.get_n(),1,0.);
+	field::Field<FLDARRAY> cutmask(mask.get_n(),1);
 
 	this->UpdateProgress(0.2);
 
 	// Loop and update cutting mask (Mesh loop, can be parallelized)
 	field::Field<FLDARRAY>::iterator it_mask, it_cut;
 	for (it_mask = mask.begin(), it_cut = cutmask.begin(); it_mask != mask.end(); ++it_mask,++it_cut) {
+		it_cut[0] = 0;
 		// Loop on the basins array selection
-		for (int bid=0; bid < this->GetNumberOfCoastsArrays(); bid++)
-			if ( this->GetCoastsArrayStatus(this->GetCoastsArrayName(bid)) &&
-				 std::fabs(it_mask[0] - (double)(bid+1)) < 1.e-3 ) 
-				it_cut[0] = 1.;
+		for (int bid=0; bid < this->GetNumberOfCoastsArrays(); ++bid)
+			// Test to zero is allowed when working with integers
+			if ( this->GetCoastsArrayStatus(this->GetCoastsArrayName(bid)) && (it_mask[0] - (bid+1)) == 0 ) {
+				it_cut[0] = 1; continue;
+			}
 	}
 
 	// Convert field to vtkArray and add it to input
