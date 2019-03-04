7d7ee875572927e94da161aa85ce020de8355a5f
Arnau Miró <arnaumiro@luvarusimperialis.inogs.it>
2019-03-04 13:28:52 +0100

    Modified OGS Q Criterion to match with the new structures. Added OpenMP parallelization

--- OGSPlugins/OGSQCriterion/vtkOGSComputeQCriterion.cxx
+++ OGSPlugins/OGSQCriterion/vtkOGSComputeQCriterion.cxx
@@ -17,7 +17,8 @@
 #include "vtkCellData.h"
 #include "vtkPointData.h"
 #include "vtkFieldData.h"
-#include "vtkFloatArray.h"
+#include "vtkTypeUInt8Array.h"
+#include "vtkDoubleArray.h"
 #include "vtkCellData.h"
 #include "vtkDataArraySelection.h"
 #include "vtkInformation.h"
@@ -28,13 +29,22 @@
 
 #include "vtkObjectFactory.h"
 
-namespace VTK
-{
-// Include the VTK Operations
-#include "../_utils/vtkOperations.cpp"
-}
-
 vtkStandardNewMacro(vtkOGSComputeQCriterion);
+
+//----------------------------------------------------------------------------
+
+/*
+	Macro to set the array precision 
+*/
+#define FLDARRAY double
+#define FLDMASK  uint8_t
+#define VTKARRAY vtkDoubleArray
+#define VTKMASK  vtkTypeUInt8Array
+
+// V3.h and field.h defined in vtkOGSDerivatives.h
+#include "../_utils/fieldOperations.hpp"
+#include "../_utils/vtkFields.hpp"
+#include "../_utils/vtkOperations.hpp"
 
 //----------------------------------------------------------------------------
 vtkOGSComputeQCriterion::vtkOGSComputeQCriterion()
@@ -78,162 +88,200 @@ int vtkOGSComputeQCriterion::RequestData(
 	// Recover a mask
 	// This is useful to perform a true average and srd on the Okubo-Weiss
 	// Either the basins or the coast mask will work
-	vtkFloatArray *vtkMask = vtkFloatArray::SafeDownCast(
-		input->GetCellData()->GetArray("coast mask"));
+	field::Field<FLDMASK> mask;
+	VTKMASK *vtkMask = VTKMASK::SafeDownCast( input->GetCellData()->GetArray("coast mask") );
 	if (!vtkMask)
-		vtkMask = vtkFloatArray::SafeDownCast(
-		input->GetCellData()->GetArray("basins mask"));
-	if (!vtkMask)
+		vtkMask = VTKMASK::SafeDownCast( input->GetCellData()->GetArray("basins mask") );
+
+	if (vtkMask)
+		mask = VTK::createFieldfromVTK<VTKMASK,FLDMASK>(vtkMask);
+	else
 		vtkWarningMacro("Cannot find a working mask!\nResults might be affected.");
 
 	// Recover weights
-	vtkFloatArray *vtke1 = vtkFloatArray::SafeDownCast(
-		input->GetCellData()->GetArray("e1"));
-	vtkFloatArray *vtke2 = vtkFloatArray::SafeDownCast(
-		input->GetCellData()->GetArray("e2"));
-	vtkFloatArray *vtke3 = vtkFloatArray::SafeDownCast(
-		input->GetCellData()->GetArray("e3"));
+	VTKARRAY *vtke1 = VTKARRAY::SafeDownCast( input->GetCellData()->GetArray("e1") );
+	VTKARRAY *vtke2 = VTKARRAY::SafeDownCast( input->GetCellData()->GetArray("e2") );
+	VTKARRAY *vtke3 = VTKARRAY::SafeDownCast( input->GetCellData()->GetArray("e3") );
 
 	if (this->grad_type > 1 && (vtke1 == NULL || vtke2 == NULL || vtke3 == NULL)) {
 		vtkErrorMacro("Mesh weights (e1, e2 and e3) need to be loaded to proceed!");
 		return 0;
 	}
 
+	// Convert to field arrays
+	field::Field<FLDARRAY> e1, e2, e3;
+	if (vtke1) e1 = VTK::createFieldfromVTK<VTKARRAY,FLDARRAY>(vtke1);
+	if (vtke2) e2 = VTK::createFieldfromVTK<VTKARRAY,FLDARRAY>(vtke2);
+	if (vtke3) e3 = VTK::createFieldfromVTK<VTKARRAY,FLDARRAY>(vtke3);
+
 	// Recover velocity vector
-	vtkFloatArray *vtkVeloc = vtkFloatArray::SafeDownCast(
-		input->GetCellData()->GetArray(this->field));
+	VTKARRAY *vtkArray;
+	vtkArray = VTKARRAY::SafeDownCast(input->GetCellData()->GetArray(this->field));
+	if (vtkArray == NULL) {
+		vtkErrorMacro("Input array "<<this->field<<"is not a cell array."<<
+			"This function can only deal with cell arrays!");
+		return 0;
+	}
 
-	// Compute the cell centers
-	vtkFloatArray *vtkCellCenters = VTK::getCellCoordinates("Cell Centers",input);
+	// Here we have a valid vtkArray. Convert it to a field.
+	field::Field<FLDARRAY> array;
+	array = VTK::createFieldfromVTK<VTKARRAY,FLDARRAY>(vtkArray);
 
-	// Generate a new scalar array to store the Okubo-Weiss and the mask
-	vtkFloatArray *vtkQ  = VTK::createVTKscaf("Q-criterion",nx-1,ny-1,nz-1,NULL);
-	vtkFloatArray *vtkQm = VTK::createVTKscaf("Q-criterion mask",nx-1,ny-1,nz-1,NULL);
+	// Cell centers, update only when request info
+	if (this->isReqInfo) {
+		this->isReqInfo = false;
+		
+		// Recover Metadata array (depth factor)
+		vtkStringArray *vtkmetadata = vtkStringArray::SafeDownCast(
+			input->GetFieldData()->GetAbstractArray("Metadata"));
+		double dfact = (vtkmetadata != NULL) ? std::stod( vtkmetadata->GetValue(2) ) : 1000.;
+		
+		if (vtkmetadata == NULL) 
+			vtkWarningMacro("Field array Metadata not found! Depth factor set to 1000. automatically.");
+
+		this->xyz = VTK::getVTKCellCenters(input,dfact);
+	}
+
+	// Loop on the 3D mesh and compute the Q criterion also new scalar arrays 
+	// to store the values for the weights and the mean
+	field::Field<FLDARRAY> Q(array.get_n(),1,0.);
+	FLDARRAY Q_mean = 0., sum_weights = 0.;
 
 	this->UpdateProgress(0.1);
 
-	// Loop the mesh
-	double Q_mean = 0., sum_weights = 0., Q_std = 0.;
-	double deri[9]     = {0.,0.,0.,0.,0.,0.,0.,0.,0.};
-	double deri_old[9] = {0.,0.,0.,0.,0.,0.,0.,0.,0.};
-
-	// Loop on the 3D mesh and compute Q-criterion
+	#pragma omp parallel for collapse(3) reduction(+:Q_mean,sum_weights)
 	for (int kk = 0; kk < nz-1; kk++) {
 		for (int jj = 0; jj < ny-1; jj++) {
 			for (int ii = 0; ii < nx-1; ii++) {
-				// Check the mask, skip the point if we are out of the mask
-				if (vtkMask && vtkMask->GetTuple1(CLLIND(ii,jj,kk,nx,ny)) < 0)
-					continue;
-				// Store the previous derivative
-				if (this->grad_type > 1) {
-					for (int dd = 0; dd < 9; dd++) 
-						deri_old[dd] = deri[dd];
+				// Compute current point
+				int ind = CLLIND(ii,jj,kk,nx,ny);
+
+				// Use the mask to determine whether we are in the sea or not
+				// to improve the averaging
+				if (!mask.isempty()) {
+					bool skip_ind = true;
+					for (int ii = 0; ii < mask.get_m(); ++ii) {
+						if (mask[ind][ii] > 0) {skip_ind = false; break;}
+					}
+					if (skip_ind) continue;
 				}
-				// Recover e1u, e2v and e3w
-				double e1[4], e2[4], e3[4];
-				vtke1->GetTuple(CLLIND(ii,jj,kk,nx,ny),e1);
-				vtke2->GetTuple(CLLIND(ii,jj,kk,nx,ny),e2);
-				vtke3->GetTuple(CLLIND(ii,jj,kk,nx,ny),e3);
-				// Selection of the gradient method
+
+				// Compute the gradient
+				FLDARRAY deri[9] = {0.,0.,0.,0.,0.,0.,0.,0.,0.};
+
 				switch (this->grad_type) {
 					case 0: // Second order, face centered gradient
 							// This gradient is unsafe as it relies on the mesh projection
-						VTK::vtkGrad3XY2(ii,jj,kk,nx-1,ny-1,nz-1,vtkVeloc,vtkCellCenters,deri);
+						field::gradXYZ2_ijk(ii,jj,kk,nx-1,ny-1,nz-1,this->xyz,array,deri);
 						break;
 					case 1: // Fourth order, face centered gradient
 							// This gradient is unsafe as it relies on the mesh projection
-						VTK::vtkGrad3XY4(ii,jj,kk,nx-1,ny-1,nz-1,vtkVeloc,vtkCellCenters,deri);
+						field::gradXYZ4_ijk(ii,jj,kk,nx-1,ny-1,nz-1,this->xyz,array,deri);
 						break;
 					case 2:	// OGSTM-BFM approach according to the NEMO handbook
 							// This gradient is safe as it relies on the code implementation
-						VTK::vtkGrad3OGS1(ii,jj,kk,nx-1,ny-1,nz-1,
-							vtkVeloc,e1[1],e2[2],e3[3],deri);
+						field::gradOGS1_ijk(ii,jj,kk,nx-1,ny-1,nz-1,array,e1,e2,e3,deri);
 						break;
 					case 3:	// 2nd order OGSTM-BFM approach
 							// This gradient is experimental
-						VTK::vtkGrad3OGS2(ii,jj,kk,nx-1,ny-1,nz-1,
-							vtkVeloc,e1[1],e2[2],e3[3],deri);
+						field::gradOGS2_ijk(ii,jj,kk,nx-1,ny-1,nz-1,array,e1,e2,e3,deri);
 						break;
 					case 4:	// 4th order OGSTM-BFM approach
 							// This gradient is experimental
-						VTK::vtkGrad3OGS4(ii,jj,kk,nx-1,ny-1,nz-1,
-							vtkVeloc,e1[1],e2[2],e3[3],deri);
+						field::gradOGS4_ijk(ii,jj,kk,nx-1,ny-1,nz-1,array,e1,e2,e3,deri);
 						break;
 				}
-				// Interpolate on the centered grid
-				if (this->grad_type > 1) {
-					// Interpolation first component
-					VTK::facecen2cellcen(ii,jj,kk,nx-1,ny-1,
-						deri_old,deri,vtke1,vtke2,vtke3,deri);
-					// Interpolation second component
-					VTK::facecen2cellcen(ii,jj,kk,nx-1,ny-1,
-						deri_old+3,deri+3,vtke1,vtke2,vtke3,deri+3);
-					// Interpolation third component
-					VTK::facecen2cellcen(ii,jj,kk,nx-1,ny-1,
-						deri_old+6,deri+6,vtke1,vtke2,vtke3,deri+6);		
-				}
+
 				// Compute Q-criterion
-				double Q = -0.5*(deri[0]*deri[0] + deri[4]*deri[4] + deri[8]*deri[8])
-						-deri[1]*deri[3]-deri[2]*deri[6]-deri[5]*deri[7];
-				Q *= -2;
+				Q[ind][0] = -0.5*(deri[0]*deri[0] + deri[4]*deri[4] + deri[8]*deri[8])
+						    -deri[1]*deri[3]-deri[2]*deri[6]-deri[5]*deri[7];
+				Q[ind][0]*= -2; // Scaling fix so that Q == OW
+
 				// Compute the mean
-				Q_mean      += Q*e1[0]*e2[0];
-				sum_weights += e1[0]*e2[0];
-				// Set Okubo-Weiss
-				vtkQ->SetTuple1(CLLIND(ii,jj,kk,nx,ny),Q);
+				Q_mean      += Q[ind][0]*e1[ind][0]*e2[ind][0];
+				sum_weights += e1[ind][0]*e2[ind][0];
 			}
 		}
-		this->UpdateProgress(0.1+0.3/(nz-1.)*kk);
 	}
-	// Up to this point the Okubo-Weiss criterion in the surface is computed
-	// and stored in vtkOW and the mean in OW_mean.
+	this->UpdateProgress(0.4);
+
+	// Work out the mean
 	Q_mean /= sum_weights;
 
-	// Now we work out the standard deviation
+	// Up to this point the Q criterion in the surface is computed.
+	// Now we can work out the standard deviation.
+	FLDARRAY Q_std = 0.;
+
+	#pragma omp parallel for collapse(3) reduction(+:Q_std)
 	for (int kk = 0; kk < nz-1; kk++) {
 		for(int jj = 0; jj < ny-1; jj++){
 			for (int ii = 0; ii < nx-1; ii++) {
-				// Check the mask, skip the point if we are out of the mask
-				if (vtkMask && vtkMask->GetTuple1(CLLIND(ii,jj,kk,nx,ny)) < 0)
-					continue;
-				double e1[4], e2[4];
-				vtke1->GetTuple(CLLIND(ii,jj,kk,nx,ny),e1);
-				vtke2->GetTuple(CLLIND(ii,jj,kk,nx,ny),e2);
-				double Q  = vtkQ->GetTuple1(CLLIND(ii,jj,kk,nx,ny));
-				Q_std    += (Q - Q_mean)*(Q - Q_mean)*e1[0]*e2[0];
+				// Compute current point
+				int ind = CLLIND(ii,jj,kk,nx,ny);
+
+				// Use the mask to determine whether we are in the sea or not
+				// to improve the averaging
+				if (!mask.isempty()) {
+					bool skip_ind = true;
+					for (int ii = 0; ii < mask.get_m(); ++ii) {
+						if (mask[ind][ii] > 0) {skip_ind = false; break;}
+					}
+					if (skip_ind) continue;
+				}
+
+				Q_std += (Q[ind][0] - Q_mean)*(Q[ind][0] - Q_mean)*e1[ind][0]*e2[ind][0];
 			}
 		}
-		this->UpdateProgress(0.4+0.3/(nz-1.)*kk);
 	}
+
+	this->UpdateProgress(0.7);
 	Q_std = this->coef*sqrt(Q_std/sum_weights);
 
-	// We now have the standard deviation, let us work out the mask for
-	// the surface
+	// We now have the standard deviation, let us work out the mask
+	field::Field<FLDMASK> Qm(array.get_n(),1);
+
+	#pragma omp parallel for collapse(3)
 	for (int kk = 0; kk < nz-1; kk++) {
 		for(int jj = 0; jj < ny-1; jj++){
 			for (int ii = 0; ii < nx-1; ii++) {
-				// Check the mask, skip the point if we are out of the mask
-				if (vtkMask && vtkMask->GetTuple1(CLLIND(ii,jj,kk,nx,ny)) < 0)
-					continue;
-				double Q  = vtkQ->GetTuple1(CLLIND(ii,jj,kk,nx,ny));
-				// Vorticity-dominated flow
-				if (Q < -Q_std)
-					vtkQm->SetTuple1(CLLIND(ii,jj,kk,nx,ny),-1.);
-				// Strain-dominated flow
-				if (Q > Q_std)
-					vtkQm->SetTuple1(CLLIND(ii,jj,kk,nx,ny),1.);
+				// Compute current point
+				int ind = CLLIND(ii,jj,kk,nx,ny);
+
+				// Initalize mask for Background field
+				Qm[ind][0] = 1;
+
+				// Use the mask to determine whether we are in the sea or not
+				// to improve the averaging
+				if (!mask.isempty()) {
+					bool skip_ind = true;
+					for (int ii = 0; ii < mask.get_m(); ++ii) {
+						if (mask[ind][ii] > 0) {skip_ind = false; break;}
+					}
+					if (skip_ind) continue;
+				}
+
+				// Set the values of the mask
+				if (Q[ind][0] < -Q_std) Qm[ind][0] = 0; // Vorticity-dominated flow
+				if (Q[ind][0] >  Q_std) Qm[ind][0] = 2; // Strain-dominated flow
 			}
 		}
-		this->UpdateProgress(0.7+0.3/(nz-1.)*kk);
 	}
+	this->UpdateProgress(0.9);
 		
-	vtkCellCenters->Delete();
-
-	// Add arrays to input
-	output->GetCellData()->AddArray(vtkQ);  vtkQ->Delete();
-	output->GetCellData()->AddArray(vtkQm); vtkQm->Delete();
+	// Generate VTK arrays and add them to the output
+	vtkArray = VTK::createVTKfromField<VTKARRAY,FLDARRAY>("Q-criterion",Q);
+	output->GetCellData()->AddArray(vtkArray);  vtkArray->Delete();
+	vtkMask = VTK::createVTKfromField<VTKMASK,FLDMASK>("Q-criterion mask",Qm);
+	output->GetCellData()->AddArray(vtkMask);  vtkMask->Delete();
 
 	// Copy the input grid
 	this->UpdateProgress(1.);
 	return 1;
+}
+
+//----------------------------------------------------------------------------
+int vtkOGSComputeQCriterion::RequestInformation(vtkInformation* vtkNotUsed(request),
+  vtkInformationVector** vtkNotUsed(inputVector), vtkInformationVector* outputVector) {
+
+  	this->isReqInfo = true;
 }
