d214a4aa7c3a68ade3071235973c4939c316d901
Arnau Miró <arnaumiro@luvarusimperialis.inogs.it>
2019-03-06 10:25:26 +0100

    A bit of code cleanup

--- OGSPlugins/OGSQCriterion/vtkOGSComputeQCriterion.cxx
+++ OGSPlugins/OGSQCriterion/vtkOGSComputeQCriterion.cxx
@@ -61,11 +61,8 @@ vtkOGSComputeQCriterion::~vtkOGSComputeQCriterion()
 }
 
 //----------------------------------------------------------------------------
-int vtkOGSComputeQCriterion::RequestData(
-  vtkInformation *vtkNotUsed(request),
-  vtkInformationVector **inputVector,
-  vtkInformationVector *outputVector)
-{
+int vtkOGSComputeQCriterion::RequestData(vtkInformation *vtkNotUsed(request),
+  vtkInformationVector **inputVector,vtkInformationVector *outputVector) {
 	// Get the info objects
 	vtkInformation *inInfo = inputVector[0]->GetInformationObject(0);
 	vtkInformation *outInfo = outputVector->GetInformationObject(0);
