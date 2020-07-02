5350efaa3a11a9b9f500fc4c9614971da17cbc46
Arnau Miró <arnaumiro@luvarusimperialis.inogs.it>
2020-07-02 16:39:45 +0200

    Fixed bug

--- superbuild/paraview_superbuild_linux_gen.sh
+++ superbuild/paraview_superbuild_linux_gen.sh
@@ -194,7 +194,7 @@ printf "OK\n"
 # Deploy OGSMesh and OGS2Paraview inside the installation
 printf "Deploying OGSMesh and OGS2Paraview... "
 mv $SUITEDIR/libOGS.so $INSTALL_PREFIX/lib
-cp $SUPERBUILD_DIR/env-linux.sh $INSTALL_PREFIX/env.sh
+cp $SUITEDIR/superbuild/env-linux.sh $INSTALL_PREFIX/env.sh
 cp $PLUGINDIR/_utils/python/OGSmesh.py $INSTALL_PREFIX/lib/python*/site-packages
 cp $PLUGINDIR/_utils/python/OGSlonlat2m.py $INSTALL_PREFIX/bin
 cp $PLUGINDIR/_utils/python/OGS2Paraview.py $INSTALL_PREFIX/bin
@@ -203,5 +203,5 @@ printf "OK\n"
 
 # Clean-up
 cd $SUITEDIR
-rm -rf $BUILD_DIR $SUPERBUILD_DIR
-tar cvzf "${INSTALL_PREFIX}.tar.gz" $INSTALL_PREFIX
+#rm -rf $BUILD_DIR $SUPERBUILD_DIR
+#tar cvzf "${INSTALL_PREFIX}.tar.gz" $INSTALL_PREFIX
