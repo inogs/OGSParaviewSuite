0e1dcc7a19e866cfd9abbe221b5969ba56f2be54
Arnau Miró <arnaumiro@luvarusimperialis.inogs.it>
2020-07-14 15:03:54 +0200

    Fixed env

--- superbuild/env-galileo.sh
+++ superbuild/env-galileo.sh
@@ -9,7 +9,7 @@ module load profile/advanced
 module load gnu/6.1.0
 module load openmpi/3.1.1--gnu--6.1.0
 
-export PATH=$PATH:$OGSSUITEDIR/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$OGSSUITEDIR/lib:$OGSSUITEDIR/lib/paraview-5.6
-export C_INCLUDE_PATH=$C_INCLUDE_PATH:$DIR/include:$DIR/include/python2.7:$DIR/include/paraview-5.6
-export PYTHONPATH=$PYTHONPATH:$OGSSUITEDIR/lib/python2.7/site-packages
+export PATH=$OGSSUITEDIR/bin:$PATH
+export LD_LIBRARY_PATH=$OGSSUITEDIR/lib:$OGSSUITEDIR/lib/paraview-5.6:$LD_LIBRARY_PATH
+export C_INCLUDE_PATH=$DIR/include:$DIR/include/python2.7:$DIR/include/paraview-5.6:$C_INCLUDE_PATH
+export PYTHONPATH=$OGSSUITEDIR/lib/python2.7/site-packages:$PYTHONPATH
